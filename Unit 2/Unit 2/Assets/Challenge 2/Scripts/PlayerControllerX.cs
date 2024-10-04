04d707a6eb9d0f5335767a954355a157fb409cd4
tgoens12 <25goent@elida.k12.oh.us>
2024-10-04 10:16:25 -0400

    Finished Unit 2

---
+++ Unit 2/Unit 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float fireDelay = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireDelay -= 1.05f * Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && fireDelay <= 0.0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            fireDelay = 1.0f;
+        }
+    }
+}
